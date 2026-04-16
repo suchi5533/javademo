0d901fe085cd3f09a405743f7e855955a5340734
suchi5533 <suchitw867@gmail.com>
2026-04-16 11:15:36 +0530

    add new code

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
     
     @GetMapping("/")
     public String root() {
-        return "Hello DevOps Suchit";
+        return "Hello DevOps Suchit-wa";
     }
     
     
